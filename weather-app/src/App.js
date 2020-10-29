d727c30b940beff26fc78f430e774e61b4721f90
Jemina Kotkajuuri <jemina.kotkajuuri@gmail.com>
2020-10-29 15:58:54 +0200

    Lisätty päivämäärähaku, varoitusviesti jos huono haku ja kommentointia

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,54 +1,65 @@
 import React, { useState } from 'react';
 import './App.css';
 import Metolib from '@fmidev/metolib'
-//
 
 function App () {
-  const [temperatures, setTemperatures] = useState([]);
-  const [times, setTimes] = useState([]);
-  const [input, setInput] = useState("");
-  let tempDay = [];
 
+  //Hooks
+  const [temperatures, setTemperatures] = useState([]);
+  const [input, setInput] = useState({city: "", date: ""});
+  const [warning, setWarning] = useState("");
+
+  //Haetaan annettujen parametrien mukainen data ilmatieteenlaitoksen latauspalvelusta
   function getData() {
     var SERVER_URL = "http://opendata.fmi.fi/wfs";
     var STORED_QUERY_OBSERVATION = "fmi::observations::weather::multipointcoverage";
     var parser = new Metolib.WfsRequestParser();
-
+    let dateParts = input.date.split('/').map(Number);
     parser.getData({
     url : SERVER_URL,
     storedQueryId: STORED_QUERY_OBSERVATION,
     requestParameter : "temperature",
-    begin : new Date(2020, 9, 20, 0, 0, 0),
-    end : new Date(2020, 9, 21, 0, 0, 0),
+    begin : new Date(dateParts[2], dateParts[1]-1, dateParts[0], 0, 0, 0),
+    end : new Date(dateParts[2], dateParts[1]-1, dateParts[0], 23, 0, 0),
     timestep : 60 * 60 * 1000,
-    sites : input,
+    sites : input.city,
     callback : function(data, errors) {
       handleData(data, errors);
     }
    });
-}
-
-function handleData(data, errors) {
-  console.log(data);
-  if (errors.length === 0) {
-    for (const pair of data.locations[0].data.temperature.timeValuePairs) {
-      var timeMs = new Date(pair.time);
-      var timeString = timeMs.toTimeString().substring(0, 5);
-      tempDay.push([timeString, pair.value]);
-    }
-    setTemperatures(tempDay);
-  } else {
-    console.log("Error");
-    return;
   }
-}
 
-function handleInput(event) {
-  event.preventDefault();
-  setInput(event.target.value);
-  console.log(event.target.value);
-}
+  //Käsitellään ja asetetaan haettu data. Jos virheitä, näytetään varoitusviesti.
+  function handleData(data, errors) {
+    console.log(data);
+    let tempDay = [];
+    if (errors.length === 0) {
+      for (const pair of data.locations[0].data.temperature.timeValuePairs) {
+        var timeMs = new Date(pair.time);
+        var timeString = timeMs.toTimeString().substring(0, 5);
+        tempDay.push([timeString, pair.value]);
+      }
+      setTemperatures(tempDay);
+      setWarning("");
+    } else {
+      console.log("Error");
+      setTemperatures([]);
+      setWarning("Tarkista hakuehdot!");
+      return;
+    }
+  }
 
+  //Funktio input-palkkien tekstinsyötön käsittelyyn
+  function handleInput(event) {
+    event.preventDefault();
+    const value = event.target.value;
+    setInput({
+      ...input,
+      [event.target.name]: value
+    });
+  }
+
+  //Käsitellään hakunapin painallus
   const handleClick = (event) => {
     event.preventDefault();
     getData();
@@ -58,16 +69,18 @@ function handleInput(event) {
     <div className="App">
       <h1>Sää</h1>
       <button onClick={handleClick}>Hae tiedot</button>
-      <input value={input} onChange={handleInput}/>
-      <div id="city">{input}</div>
-      <div id="date">12/10/2020</div>
-      <div className="current">
+      <input name="city" value={input.city} onChange={handleInput} placeholder="kaupunki" />
+      <input name="date" value={input.date} onChange={handleInput} placeholder="pp/kk/vvvv" />
+      <h3>{warning}</h3>
+      <div id="city">{input.city}</div>
+      <div id="date">{input.date}</div>
+      <div className="weatherData">
         <header>WEATHER</header>
         <div className="weatherBar">
         {temperatures.map((item, index) => (
-          <div className="temp">
-            <header className="time">{item[0]}</header>
-            <div className="t">{item[1]}</div>
+          <div key={index + "div"} className="temp">
+            <header key={index + "time"} className="time">{item[0]}</header>
+            <div key={index + "t"} className="t">{item[1]}</div>
           </div>
         ))}
         </div>
