92714a11c061146569a3e4bbf6627f196dc3a21c
Jemina Kotkajuuri <jemina.kotkajuuri@gmail.com>
2020-11-09 14:00:43 +0200

    Lisätty tuulennopeus grafiikkaa

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-import Metolib from '@fmidev/metolib'
+import Metolib from '@fmidev/metolib';
+import * as d3 from 'd3';
 
 function App () {
 
@@ -8,6 +9,51 @@ function App () {
   const [temperatures, setTemperatures] = useState([]);
   const [input, setInput] = useState({city: "", date: ""});
   const [warning, setWarning] = useState("");
+  const [windData, setWindData] = useState([]);
+  const [windTimeData, setWindTimeData] = useState([]);
+  const svgRef = useRef();
+
+  //Efekti, jota käytetään tuulen nopeuden grafiikan näyttämiseen
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+
+    //X-akselin skaalaus
+    const xScale = d3.scaleLinear()
+      .domain([0, windData.length-1])
+      .range([0,300]);
+
+    //Y-akselin skaalaus
+    const yScale = d3.scaleLinear()
+      .domain([0, 10])
+      .range([150, 0]);
+
+    //X-akseli
+    const xAxis = d3.axisBottom(xScale)
+      .ticks(windData.length)
+      .tickFormat(index => windTimeData[index]);
+    svg.select(".x-axis")
+      .style("transform", "translateY(150px)")
+      .call(xAxis);
+    //Y-akseli
+    const yAxis = d3.axisRight(yScale)
+      .tickFormat(value => value + " m/s");
+    svg.select(".y-axis")
+      .style("transform", "translateX(300px)")
+      .call(yAxis);
+    //Viiva
+    const windLine = d3.line()
+      .x((value, index) => xScale(index))
+      .y(yScale)
+      .curve(d3.curveCardinal);
+    svg
+      .selectAll(".line")
+      .data([windData])
+      .join("path")
+      .attr("class", "line")
+      .attr("d", windLine)
+      .attr("fill", "none")
+      .attr("stroke", "blue");
+  }, [windData, windTimeData]);
 
   //Haetaan annettujen parametrien mukainen data ilmatieteenlaitoksen latauspalvelusta
   function getData() {
@@ -41,10 +87,52 @@ function App () {
       }
       setTemperatures(tempDay);
       setWarning("");
+      getWindData();
     } else {
       console.log("Error");
       setTemperatures([]);
       setWarning("Tarkista hakuehdot!");
+      return;
+    }
+  }
+
+  //Haetaan annettujen parametrien mukainen tuulidata ilmatieteenlaitoksen latauspalvelusta
+  function getWindData() {
+    var SERVER_URL = "http://opendata.fmi.fi/wfs";
+    var STORED_QUERY_OBSERVATION = "fmi::observations::weather::multipointcoverage";
+    var parser = new Metolib.WfsRequestParser();
+    let dateParts = input.date.split('/').map(Number);
+    parser.getData({
+    url : SERVER_URL,
+    storedQueryId: STORED_QUERY_OBSERVATION,
+    requestParameter : "ws_10min",
+    begin : new Date(dateParts[2], dateParts[1]-1, dateParts[0], 0, 0, 0),
+    end : new Date(dateParts[2], dateParts[1]-1, dateParts[0], 23, 0, 0),
+    timestep : 60 * 60 * 1000 * 3,
+    sites : input.city,
+    callback : function(data, errors) {
+      handleWindData(data, errors);
+    }
+   });
+  }
+
+  //Käsitellään ja asetetaan haettu data. Jos virheitä, näytetään varoitusviesti.
+  function handleWindData(data, errors) {
+    if (errors.length === 0) {
+      let windAr = [];
+      let windTimeAr = [];
+      for (const pair of data.locations[0].data.ws_10min.timeValuePairs) {
+        var timeMs = new Date(pair.time);
+        var timeString = timeMs.toTimeString().substring(0, 5);
+        windTimeAr.push([timeString]);
+        windAr.push([pair.value]);
+      }
+      setWindData(windAr);
+      setWindTimeData(windTimeAr);
+    } else {
+      console.log("Error");
+      setWindData([]);
+      setWindTimeData([]);
       return;
     }
   }
@@ -83,6 +171,12 @@ function App () {
               <div key={index + "t"} className="t">{item[1]}</div>
             </div>
           ))}
+        </div>
+        <div>
+          <svg ref={svgRef}>
+            <g className="x-axis" />
+            <g className="y-axis" />
+          </svg>
         </div>
       </div>
       <div className="weatherData">
