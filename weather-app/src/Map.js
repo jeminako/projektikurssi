4dd0bd6e514df8469bbd37a76f27106f8c933753
Pyry L <pyry.lappalainen@kave.fi>
2020-11-24 13:43:55 +0200

    Lisätty leaflettiin vaadittavat packaget, ja tehty yksinkertainen karttanäkymä

--- weather-app/src/Map.js
+++ weather-app/src/Map.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import './App.css';
+import './Map.css';
+import { Map as LeafletMap, Marker, Popup, TileLayer } from "react-leaflet";
 import Metolib from '@fmidev/metolib';
 
 function Map() {
+
+    const KOMPASSI = [62.242631, 25.747356];
+
+    const [clickPos, setClickPos] = React.useState([62.242631, 25.747356]);
+
+    function mapClick(e) {
+        setClickPos([e.latlng.lat, e.latlng.lng]);
+        console.log(e.latlng);
+    }
+
     return (
         <div className='App'>
             <h1>Map</h1>
+            <LeafletMap center={KOMPASSI} zoom={4} onclick={mapClick}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={clickPos}>
+                    <Popup>A pretty CSS3 popup. <br /> Easily customizable.</Popup>
+                </Marker>
+            </LeafletMap>
         </div>
     );
 }
