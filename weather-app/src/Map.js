49de9ca2bebb2e193848afe52ce24db936e83dbd
Pyry L <pyry.lappalainen@kave.fi>
2020-11-25 20:55:51 +0200

    Lisätty virheviesti huonoista hauista (ei-Suomessa/epävalidi vastaus)

--- weather-app/src/Map.js
+++ weather-app/src/Map.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import './Map.css';
+import './App.css';
 import { Map as LeafletMap, Marker, Popup, TileLayer } from "react-leaflet";
 //import Metolib from '@fmidev/metolib';
 
 function Map() {
 
+    /** JKL kompassi koords :D */
     const KOMPASSI = [62.242631, 25.747356];
 
+    /** Erilaisia tyyppjeä joita nominativin antamassa address-oliossa voi olla nimettynä */
     const PLACE_TYPES = ["town", "city", "municipality", "county"];
 
     const [clickPos, setClickPos] = useState(KOMPASSI);
     const [inputCity, setInputCity] = useState("Jyväskylä");
+    const [selectedLocation, setSelectedLocation] = useState("Jyväskylä");
     const [inputDate, setInputDate] = useState("");
+    const [searchError, setSearchError] = useState("");
 
+    /** Hakee JSONP-objektin URLstä
+     * @param {Strgin} url URL jolla haetaan
+     * @param {Function} success Mitä tehdään kun JSON-haku onnistui
+     */
     function getJSONP(url, success) {
 
         var ud = '_' + +new Date();
@@ -31,32 +40,74 @@ function Map() {
 
     function mapClick(e) {
         setClickPos([e.latlng.lat, e.latlng.lng]);
-        console.log(e.latlng);
 
         //Hae paikka
         getJSONP(
             'https://nominatim.openstreetmap.org/reverse?lat=' + e.latlng.lat + "&lon=" + e.latlng.lng + "&format=jsonv2&json_callback=?",
             function (data) {
-                console.log(data);
+                //console.log(data);
+
+                var error = "";
+                /** Löytyikö paikalle nimi */
+                var loytyi = false;
 
                 if (data === undefined || data === null || data.address === undefined || data.address === null) {
-                    //
+                    //Ei validi vastaus
+                    error = "Paikkatietoja ei ole saatavilla";
                 }
                 else {
                     let addr = data.address;
-    
+
+                    if (data.address.country_code !== "fi") {
+                        error = "Säätiedot saatavilla vain Suomessa";
+                    }
+
                     for (let pt of PLACE_TYPES) {
                         if (addr[pt] !== undefined) {
+                            setSelectedLocation(addr[pt]);
                             setInputCity(addr[pt]);
-                            console.log(addr[pt]);
-                            return;
+                            loytyi = true;
+                            break;
                         }
                     }
                 }
-                //Aseta koords
-                setInputCity("[" + (Math.round(e.latlng.lat * 100000) / 1000000) + ", " + (Math.round(e.latlng.lng * 100000) / 1000000) + "]");
+
+                if (!loytyi) {
+                    //Aseta koords
+                    let s = latLngToString(e.latlng);
+                    setSelectedLocation(s);
+                    setInputCity(s);
+                }
+
+                setSearchError(error);
             }
         );
+    }
+
+    /** Muuttaa latlng-olion merkkijonoksi 
+     * @param {*} latlng LatLng-olio
+     * @param {Number} decimals Montako desimaalia, default 5
+     */
+    function latLngToString(latlng, decimals = 5) {
+        return coordsToString([latlng.lat, latlng.lng], decimals);
+    }
+
+
+    /** Muuttaa latlng-taulukon merkkijonoksi 
+     * @param {Number[]} latlng LatLng-taulukko
+     * @param {Number} decimals Montako desimaalia, default 5
+     */
+    function coordsToString(coords, decimals = 5) {
+        return "[" + roundToDecimals(coords[0], decimals) + ", " + roundToDecimals(coords[1], decimals) + "]";
+    }
+
+    /** Pyöristää luvun n desimaaliin
+     * @param {Number} num Pyöristettävä luku
+     * @param {Number} decimals Montako desimaalia? Default 2
+     */
+    function roundToDecimals(num, decimals = 2) {
+        let mult = Math.pow(10, decimals);
+        return Math.round(num * mult) / mult;
     }
 
     /** Asettaa tekstivaluen
@@ -108,12 +159,13 @@ function Map() {
                                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                                 />
                                 <Marker position={clickPos}>
-                                    <Popup>A pretty CSS3 popup. <br /> Easily customizable.</Popup>
+                                    <Popup>{selectedLocation}<br />{clickPos}</Popup>
                                 </Marker>
                             </LeafletMap>
                         </td>
                         <td id="mapInfoCell">
-                            <h2>{inputCity}</h2>
+                            <h2>{selectedLocation}</h2>
+                            <div className="error">{searchError}</div>
                         </td>
                     </tr>
                 </tbody>
